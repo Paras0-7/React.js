ee5f89d5c893d805b90643977eb981036e4bfef3
Paras0-7 <parasrawat937@gmail.com>
2022-05-17 11:34:11 +0530

    two way binding

--- expense-calculator/src/components/Expenses/Expenses.js
+++ expense-calculator/src/components/Expenses/Expenses.js
@@ -1,13 +1,23 @@
+import { useState } from "react";
 import { Card } from "../UI/Card";
+import ExpensesFilter from "./ExpenseFilter";
 import { ExpenseItem } from "./ExpenseItem.js";
 import "./Expenses.css";
 export function Expense(props) {
+  const [year, setYear] = useState("");
+  const saveYearHandler = function (yearData) {
+    setYear(yearData);
+  };
+  console.log("Year Data: : ", year);
   return (
-    <Card className="expenses">
-      <ExpenseItem expense={props.expenses[0]} />
-      <ExpenseItem expense={props.expenses[1]} />
-      <ExpenseItem expense={props.expenses[2]} />
-      <ExpenseItem expense={props.expenses[3]} />
-    </Card>
+    <div>
+      <ExpensesFilter onYearSubmission={saveYearHandler} />
+      <Card className="expenses">
+        <ExpenseItem expense={props.expenses[0]} />
+        <ExpenseItem expense={props.expenses[1]} />
+        <ExpenseItem expense={props.expenses[2]} />
+        <ExpenseItem expense={props.expenses[3]} />
+      </Card>
+    </div>
   );
 }
