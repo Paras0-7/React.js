3d7d821de12075097700c0b4d5721e3869d4dc35
Paras0-7 <parasrawat937@gmail.com>
2022-05-19 13:18:12 +0530

    css modules

--- todo-list/src/components/CourseGoals/CourseInput/CourseInput.js
+++ todo-list/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,29 +1,60 @@
-import "./CourseInput.css";
 import { Button } from "../../UI/Button";
 import { useState } from "react";
+import styles from "./CourseInput.module.css";
+
+// const FormControl = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   margin: 0.5rem 0;
+//   gap: 1rem;
+
+//   & label {
+//     font-weight: bold;
+//     color: ${(props) => (props.invalid ? "red" : "black")};
+//   }
+//   & input {
+//     width: 100%;
+//     border: 1px solid ${(props) => (props.invalid ? "red" : "black")};
+//     background: ${(props) => (props.invalid ? "#ffd7d7" : "transparent")};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+// `;
 export const CourseInput = function (props) {
   const [goal, setGoal] = useState("");
-
+  const [isValid, setIsValid] = useState(true);
   const goalInputHandler = function (e) {
+    if (goal.trim().length > 0) setIsValid(true);
     setGoal(e.target.value);
   };
 
   const formSubmitHandler = function (e) {
     e.preventDefault();
-    if (goal === "") return;
+    if (goal.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
 
     const newGoal = {
       text: goal,
       id: Math.random().toString(),
     };
-
+    setIsValid(true);
+    setGoal("");
     props.getGoalData(newGoal);
   };
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="course-form">
+      <div className={`${styles["course-form"]} ${!isValid && styles.invalid}`}>
         <label>Course Goal</label>
-        <input type="text" onChange={goalInputHandler} />
+        <input type="text" value={goal} onChange={goalInputHandler} />
         <Button type="submit">Add Goal</Button>
       </div>
     </form>
--- todo-list/src/components/UI/Button.js
+++ todo-list/src/components/UI/Button.js
@@ -1,8 +1,36 @@
-import "./Button.css";
+import styles from "./Button.module.css";
+// import styled from "styled-components";
+
+// export const Button = styled.button`
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background-color: #8b005d;
+//   cursor: pointer;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   font-size: 1rem;
+//   transition: all 0.5s;
+//   width: 100%;
+
+//   &:focus {
+//     outline: none;
+//   }
+
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+
+//   @media (min-width: 768px) {
+//     width: 30%;
+//   }
+// `;
 
 export const Button = function (props) {
   return (
-    <button type={props.type} className="button">
+    <button type={props.type} className={styles.button}>
       {props.children}
     </button>
   );
