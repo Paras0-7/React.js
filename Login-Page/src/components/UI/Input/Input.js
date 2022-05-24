72171414597d73efa05c2cc81b0afe0617eb5379
Paras0-7 <parasrawat937@gmail.com>
2022-05-24 13:15:13 +0530

    context-api

---
+++ Login-Page/src/components/UI/Input/Input.js
@@ -0,0 +1,20 @@
+import classes from "./input.module.css";
+
+export const Input = function (props) {
+  return (
+    <div
+      className={`${classes.control} ${
+        props.isValid === false ? classes.invalid : ""
+      }`}
+    >
+      <label htmlFor="email">{props.label}</label>
+      <input
+        type={props.type}
+        id={props.id}
+        value={props.value}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+      />
+    </div>
+  );
+};
