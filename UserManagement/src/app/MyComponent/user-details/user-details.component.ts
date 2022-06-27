8a329e73a535d51aabf5169411e82e077fb0f319
Paras0-7 <parasrawat937@gmail.com>
2022-06-27 21:36:48 +0530

    user-details

--- UserManagement/src/app/MyComponent/user-details/user-details.component.ts
+++ UserManagement/src/app/MyComponent/user-details/user-details.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from 'src/app/MyServices/users.service';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
-  styleUrls: ['./user-details.component.css']
+  styleUrls: ['./user-details.component.css'],
 })
 export class UserDetailsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  users: Array<any> = [];
+  showdata_id: string = '';
+  constructor(private user: UsersService) {
+    this.users = this.user.getData();
+  }
+  showDetails(id: string) {
+    this.showdata_id = id;
   }
 
+  ngOnInit(): void {}
 }
--- UserManagement/src/app/MyModules/manage/manage.module.ts
+++ UserManagement/src/app/MyModules/manage/manage.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { UserDetailsComponent } from 'src/app/MyComponent/user-details/user-details.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [UserDetailsComponent],
+  imports: [CommonModule],
+  exports: [UserDetailsComponent],
 })
-export class ManageModule { }
+export class ManageModule {}
