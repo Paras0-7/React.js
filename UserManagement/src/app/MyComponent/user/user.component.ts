6946edd2c320164258bc2fa65971d9df22d66e22
Paras0-7 <parasrawat937@gmail.com>
2022-06-27 20:12:43 +0530

    events

--- UserManagement/src/app/MyComponent/user/user.component.ts
+++ UserManagement/src/app/MyComponent/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from 'src/app/MyServices/users.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -8,7 +9,8 @@ import { UsersService } from 'src/app/MyServices/users.service';
 })
 export class UserComponent implements OnInit {
   users: Array<any> = [];
-  constructor(private user: UsersService) {
+  type: boolean = false;
+  constructor(private user: UsersService, private router: Router) {
     this.users = this.user.getData();
   }
 
@@ -18,6 +20,11 @@ export class UserComponent implements OnInit {
 
   activateUser(id: any) {
     this.user.activateUser(id);
+  }
+  hasRoute(route: string) {
+    // console.log(this.router.url === '/');
+
+    return this.router.url.includes(route);
   }
   ngOnInit(): void {}
 }
