326cd2be94fcc529cef33dceaec3e6ea59b1d217
Paras Rawat <paras_rawat@epam.com>
2022-06-28 22:21:48 +0530

    readme

--- 2.7/src/test.spec.js
+++
@@ -1,7 +0,0 @@
-describe("YouTube Api", function () {
-  it("API results length is as expected", function () {
-    return fetchVideos("React").then(function (result) {
-      expect(result.length).toEqual(15);
-    });
-  });
-});
