ac78b7dfcc1a10d011ea527ff577e15223998044
Paras0-7 <parasrawat937@gmail.com>
2022-07-04 20:03:36 +0530

    router

--- React-Router/src/pages/NewQuotes.js
+++ React-Router/src/pages/NewQuotes.js
@@ -1,8 +1,10 @@
 import QuoteForm from "./../components/quotes/QuoteForm";
-
+import { useHistory } from "react-router-dom";
 export const NewQuotes = function () {
+  const history = useHistory();
   const addQuoteHandler = function (quoteData) {
     console.log(quoteData);
+    history.push("/quotes");
   };
   return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
